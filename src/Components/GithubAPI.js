53c8443ef4f3548407fe774906eafd12b8996254
Kachanov <mr.kachanov@gmail.com>
2018-09-14 19:30:29 +0300

    Added Flow

---
+++ src/Components/GithubAPI.js
@@ -0,0 +1,20 @@
+// @flow
+
+import React from 'react';
+
+import '../App.css';
+
+type Props = {};
+type State = {};
+
+class GithubAPI extends React.Component<Props, State> {
+     render() {
+        return (
+            <div className="App">
+                <h1>Github API Test</h1>
+            </div>
+        );
+    }
+}
+
+export default GithubAPI;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import GithubAPI from './Components/GithubAPI';
+
+const Root = document.getElementById("root");
+if (Root === null) {
+    throw new Error("error")
+}
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<GithubAPI />, Root);
 
