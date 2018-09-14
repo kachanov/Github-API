70169d934cc25901ffdb38c219555f16b4b194a4
Kachanov <mr.kachanov@gmail.com>
2018-09-14 20:03:17 +0300

    Added simple GUI

--- src/Components/GithubAPI.js
+++ src/Components/GithubAPI.js
@@ -1,8 +1,12 @@
 // @flow
 
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-import '../App.css';
+import styles from './GithubAPI.css';
+
 
 type Props = {};
 type State = {};
@@ -10,8 +14,22 @@ type State = {};
 class GithubAPI extends React.Component<Props, State> {
      render() {
         return (
-            <div className="App">
-                <h1>Github API Test</h1>
+            <div>
+                <div>
+                    <Typography
+                        variant="display2"
+                        className={styles.mainLabel}
+                        color="primary"
+                    >
+                        Github API Test
+                    </Typography>
+                </div>
+                <div className={styles.input}>
+                    <TextField type="text" placeholder="Enter a username" ref="username" />
+                    <Button variant="contained" color="primary" className={styles.searchButton} >
+                        Search user
+                    </Button>
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import ReactDOM from 'react-dom';
 
 import GithubAPI from './Components/GithubAPI';
 
+
 const Root = document.getElementById("root");
 if (Root === null) {
     throw new Error("error")
 }
-
 
 ReactDOM.render(<GithubAPI />, Root);
 
