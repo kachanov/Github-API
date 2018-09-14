5a58b4fe9ea15933d19085cdd547bc32452fb741
Kachanov <mr.kachanov@gmail.com>
2018-09-14 22:15:56 +0300

    Added simple request

--- src/Components/GithubAPI.js
+++ src/Components/GithubAPI.js
@@ -5,13 +5,53 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import { User } from "../types/userType";
+
 import styles from './GithubAPI.css';
 
 
 type Props = {};
-type State = {};
+type State = {
+    user: User,
+};
 
 class GithubAPI extends React.Component<Props, State> {
+    input :?HTMLInputElement;
+
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            user: {
+                name: "",
+                location: "",
+                avatarURL: "",
+                repositoriesURL: "",
+                repositoriesNames: [],
+            }
+        }
+    }
+
+
+    getUserInfo = () => {
+        const username = this.input.value;
+
+        fetch(`https://api.github.com/users/${username}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    user: {
+                        name: data.name,
+                        location: data.location,
+                        avatarURL: data.avatar_url,
+                        reposURL: data.repos_url,
+                    }
+                });
+
+                console.log(this.state);
+            })
+    };
+
      render() {
         return (
             <div>
@@ -25,8 +65,17 @@ class GithubAPI extends React.Component<Props, State> {
                     </Typography>
                 </div>
                 <div className={styles.input}>
-                    <TextField type="text" placeholder="Enter a username" ref="username" />
-                    <Button variant="contained" color="primary" className={styles.searchButton} >
+                    <TextField
+                        type="text"
+                        placeholder="Enter a username"
+                        inputRef={input => (this.input = input)}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className={styles.searchButton}
+                        onClick={this.getUserInfo}
+                    >
                         Search user
                     </Button>
                 </div>
---
+++ src/types/userType.js
@@ -0,0 +1,9 @@
+export type User = {
+    user: {
+        name: String,
+        location: String,
+        avatarURL: String,
+        repositoriesURL: String,
+        repositoriesNames: Array<String>,
+    }
+}
