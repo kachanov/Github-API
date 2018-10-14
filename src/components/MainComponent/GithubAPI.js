88183b3bb7fff03805e873453796eaef050bfc5b
Kachanov <mr.kachanov@gmail.com>
2018-10-14 11:31:04 +0300

    Imported react router

--- src/components/MainComponent/GithubAPI.js
+++ src/components/MainComponent/GithubAPI.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import { Switch, Link, Route } from "react-router-dom";
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
+import { Switch, Link, Route, BrowserRouter } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
 
 import GithubAPI from './components/MainComponent/GithubAPI';
 import { store } from "./store/store";
 
+
+const history = createBrowserHistory();
 const Root = document.getElementById("root");
 if (Root === null) {
     throw new Error("Error")
 }
 
 ReactDOM.render(
-    <Provider store={store}>
-        <GithubAPI />
-    </Provider>,
+    <BrowserRouter history={history}>
+        <Provider store={store}>
+            <GithubAPI />
+        </Provider>
+    </BrowserRouter>,
     Root
 );
 
