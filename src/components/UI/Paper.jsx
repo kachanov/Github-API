df016cb3258aaf654aa1a5fcf637e42ae0f26425
Kachanov <mr.kachanov@gmail.com>
2019-08-29 23:55:52 +0300

    refactor: add min-width & min-height for Paper

--- src/components/UI/Paper.jsx
+++ src/components/UI/Paper.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 const Paper = styled.div`
+  min-width: 400px;
+  min-height: 200px;
   background-color: #daf3a9;
   box-shadow: 10px 10px 25px -8px rgba(0, 0, 0, 0.5);
 `;
