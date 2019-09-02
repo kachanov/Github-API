84ffcdb8f44b6570dc36a739f9c70c7c10e012e0
Kachanov <mr.kachanov@gmail.com>
2019-09-03 00:32:38 +0300

    refactor: fix route path

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 import App from './components/App/App';
+import { ROUTES } from './routes';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Redirect replace exact from="/" to="/home" />
-      <Route path="/home" component={App} />
+      <Redirect replace exact from="/" to={ROUTES.HOME} />
+      <Route path={ROUTES.HOME} component={App} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
